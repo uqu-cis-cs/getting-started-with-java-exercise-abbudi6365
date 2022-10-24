571f2e5ed0f09a4d0b19ad3c77a0e72e90d0aef9
abbudi6365 <s443011904@st.uqu.edu.sa>
2022-10-25 01:27:56 +0300

    lab1

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -15,6 +15,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello: <Abdullah>, <443011904>");
+        System.out.println("Hello: Abdullah, 443011904");
     }
 }
